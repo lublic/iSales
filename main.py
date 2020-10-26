0e7de4de569e4f97806bb287adcd3c0ef12978b2
lublic <71708694+lublic@users.noreply.github.com>
2020-10-26 23:12:10 +0100

    26.10.2020

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from flask import Flask
 from flask import render_template
+import daten
 
-app = Flask("Hello World")
+app = Flask("iSales")
 
 
 @app.route('/hello')
 def hello_world():
     return render_template('index.html', name="Fabian")
+
+@app.route('/umsatz')
+def get_revenue():
+    revenues = daten.aktivitaeten_laden()
+
+    return render_template('index.html', revenues=revenues)
+
+@app.route("/speichern/<aktivitaet>")
+def speichern(aktivitaet):
+    zeitpunkt, aktivitaet = daten.aktivitaet_speichern(aktivitaet)
+
+    return "Gespeichert: " + "etwas" + " um " + str("jetzt")
+
+@app.route("/liste")
+def auflisten():
+    aktivitaeten = daten.aktivitaeten_laden()
+
+    aktivitaeten_liste = ""
+    for key, value in aktivitaeten.items():
+        zeile = str(key) + ": " + value + "<br>"
+        aktivitaeten_liste += zeile
+
+    return aktivitaeten_liste
+
+
 
 
 if __name__ == "__main__":
